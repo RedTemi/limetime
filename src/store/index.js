cf923799cae5441f44aa789f5ea5722221c537c1
redtemi <tmly16@gmail.com>
2021-09-05 15:19:37 +0100

    ready

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,9 @@ export default createStore({
   state: {
     products:[],
     categories:[],
-    page:null
+    page:null,
+      pages:[],
+    selected:'all'
   },
   mutations: {
     addProducts(state,products){
@@ -13,18 +15,34 @@ export default createStore({
       products.data.forEach(element=>{
         catnames.push(element.catname);
       });
+      let pages= [];
+      for (let i = 1; i <= products.last_page; i++) {
+        pages.push(i);
+      }
+      state.pages = pages;
       state.page = products.current_page;
       state.categories = [... new Set(catnames)];
+    },
 
-    }
   },
   actions: {
     getProducts ({commit}) {
-      axios.get('https://trayvonnorthern.com/Edgewood-API/public/api/products').then((response)=>{
-        commit('addProducts',response.data)
+        axios.get('https://trayvonnorthern.com/Edgewood-API/public/api/products').then((response)=>{
+          commit('addProducts',response.data)
+        })
+      },
+    nextPage(context){
+      axios.get(context.state.products.next_page_url).then(response=>{
+        context.commit('addProducts',response.data)
+      })
+    },
+    prevPage(context){
+      axios.get(context.state.products.prev_page_url).then(response=>{
+        context.commit('addProducts',response.data)
       })
     }
-  },
+    }
+  ,
   modules: {
 
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,21 @@
 <template>
-  <h2>Products</h2>
-  <select @change="fil" class="ring ring-blue-400 bg-blue-300 text-gray-700 font-semibold font-serif px-2 py-1 rounded-lg w-2/5 outline-none mx-auto block" v-model="selected" id="">
+  <h2 class="text-center text-2xl font-bold my-4">Products</h2>
+  <select class="ring mb-4 ring-blue-400 bg-blue-300 text-gray-700 font-semibold font-serif px-2 py-1 rounded-lg w-2/5 outline-none mx-auto block" v-model="selected" id="">
     <option value="all">Showing all</option>
     <option v-for="option in categories" :key="option"  :value="option">{{option}}</option>
   </select>
     <div class="flex flex-row flex-wrap md:w-11/12 lg:w-10/12 mx-auto" >
-      <productView :products="selected === 'all'?products:productss" />
+      <productView :products="productss" />
     </div>
+  <div class="flex justify-center text-center">
+    <span v-if="products.current_page > 1" @click="prevPage"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
+</svg></span>
+    <span v-for="pagex in pages" :key="pagex" :class="{'text-blue-400': pagex === page}">{{pagex}}</span>
+    <button v-if="page<3" @click="nextPage" class="border-2 border-red-500"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+</svg></button>
+  </div>
 </template>
 
 <script>
@@ -19,35 +28,39 @@ export default {
   data(){
     return{
       selected:'all',
-      productss:null
     }
   },
   computed:{
     products() {
       return this.$store.state.products;
     },
+    pages(){
+      return this.$store.state.pages;
+    },
     page(){
       return this.$store.state.page;
     },
     categories(){
       return this.$store.state.categories;
+    },
+    productss(){
+        let temp = {};
+        Object.assign(temp,this.$store.state.products);
+        this.selected !== "all"? temp.data = temp.data.filter(element => element.catname === this.selected):null;
+      return temp;
     }
   },
   mounted(){
     this.$store.dispatch('getProducts')
+
   },
   methods:{
-    fil(){
-      if(this.selected !== "all"){
-        let temp = {};
-        Object.assign(temp,this.$store.state.products);
-        temp.data = temp.data.filter(element => element.catname === this.selected)
-        this.productss = temp;
-      }else{
-        this.productss = null;
-      }
-
-    }
+  nextPage(){
+    this.$store.dispatch('nextPage')
+  },
+  prevPage(){
+    this.$store.dispatch('prevPage')
+  }
   },
   components: {
     ProductView
